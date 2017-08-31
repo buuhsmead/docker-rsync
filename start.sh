f28a420e64d6d78d6710714b64e0482ce113db58
Ed Seymour <eseymour@redhat.com>
2017-09-01 00:16:25 +0100

    remove chmod and chown commands

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ echo "*** Using config ${CONFIG_FILE}"
 
 [ -f ${CONFIG_FILE} ] && cat ${CONFIG_FILE} >> /etc/rsyncd.conf
 
-chmod o-w,g-w -R /modules/*
-chown rsync:rsync -R /modules/*
+#chmod o-w,g-w -R /modules/*
+#chown rsync:rsync -R /modules/*
 
 /usr/bin/rsync --daemon --no-detach --config=/etc/rsyncd.conf --log-file=/var/log/rsyncd.log -v
